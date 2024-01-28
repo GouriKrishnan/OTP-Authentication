f1814f637a0a7f6198d4792b05b49534cd1c4c60
GouriKrishnan <145138217+GouriKrishnan@users.noreply.github.com>
2024-01-28 17:04:16 +0530

    OTPModel.js

---
+++ Server/models/OTPModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const otpSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  otp: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    expires: 600,
+  },
+});
+
+const OTPModel = mongoose.model('OTP', otpSchema);
+
+module.exports = OTPModel;
