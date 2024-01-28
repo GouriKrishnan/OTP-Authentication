68fa630cbcfef2e70d9e5e9801dc655c608b4f02
GouriKrishnan <145138217+GouriKrishnan@users.noreply.github.com>
2024-01-28 16:56:15 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import EmailForm from './ui-components/EmailForm.jsx';
+import OTPForm from './ui-components/OTPForm.jsx';
+import { Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <Routes>
+    <Route path="/" element={<EmailForm/>}/>
+    <Route path="/OTPForm" element={<OTPForm/>}/>
+   </Routes>
+   </>
   );
 }
 
 export default App;
+
